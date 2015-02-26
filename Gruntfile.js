e2d8303d105bd5395be20d73f27ca2db158d826c
Joshua Richey <joshua@idealist.org>
2015-02-25 22:32:22 -0800

    adding grunt for sass

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: ['scss/**/*.{scss,sass}'],
+                tasks: ['sass:dist']
+            },
+            livereload: {
+                files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
+        }
+    });
+    grunt.registerTask('default', ['sass:dist', 'watch']);
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
