0dc52eafdc268b9b349714c3192ca1e78a214ed8
Joshua Richey <joshua@idealist.org>
2015-02-27 21:59:13 -0800

    Hello.

--- app/home/Home.jsx
+++ app/home/Home.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 var Component = React.createClass({
   render: function () {
     return (
-      <h1>Coming eventually...</h1>
+      <h1>Hello.</h1>
     );
   }
 });
