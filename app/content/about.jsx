c24d93a1012e87c164ea834d80c3a21a3b2a8df1
Joshua Richey <joshua@idealist.org>
2015-07-04 16:39:53 -0700

    About page copy update.

--- app/content/about.jsx
+++ app/content/about.jsx
@@ -6,7 +6,7 @@ var About = React.createClass({
     return (
       <div className="about text-measure intro">
         <h1>joshuar</h1>
-        <p>AKA Josh, or Joshua Richey. A designer and visual craftsman living in Portland, Oregon, and
+        <p>AKA Josh, or Joshua Richey. A designer, front-end developer, and visual craftsman living in Portland, Oregon, and
           working at <a href="http://idealist.org" target="_blank">Idealist</a>.
         </p>
       </div>
