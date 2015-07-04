67742bdbd3caad09852ec5033afa242f20ff2b4b
Joshua Richey <joshua@idealist.org>
2015-07-04 16:38:23 -0700

    About me links.

--- app/content/about.jsx
+++ app/content/about.jsx
@@ -1,16 +1,63 @@
 var React = require('react');
+import styles from '../styles/about.css';
 
 var About = React.createClass({
-  render: function () {
+  render: function() {
     return (
-        <div className="about text-measure intro">
-          <h1>joshuar</h1>
-          <p>AKA Josh, or Joshua Richey. A designer and visual craftsman living in Portland, Oregon, and
-            working at <a href="http://idealist.org" target="_blank">Idealist</a>.
-          </p>
-        </div>
+      <div className="about text-measure intro">
+        <h1>joshuar</h1>
+        <p>AKA Josh, or Joshua Richey. A designer and visual craftsman living in Portland, Oregon, and
+          working at <a href="http://idealist.org" target="_blank">Idealist</a>.
+        </p>
+      </div>
     );
   }
 });
 
-module.exports = About;
+var Links = React.createClass({
+  render: function() {
+    return (
+      <div className="about-links">
+        <ul className="about-links-list">
+          <li className="about-link-item">
+            <a href="http://www.idealist.org/view/user/Jn9FMxcwskMD/"
+              target="_blank">
+              Idealist
+            </a>
+          </li>
+          <li className="about-link-item">
+            <a href="https://instagram.com/joshrr/"
+              target="_blank">
+              Instagram
+            </a>
+          </li>
+          <li className="about-link-item">
+            <a href="https://www.linkedin.com/in/joshuarichey"
+              target="_blank">
+              LinkedIn
+            </a>
+          </li>
+          <li className="about-link-item">
+            <a href="https://github.com/joshuarrr"
+              target="_blank">
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var AboutPage = React.createClass({
+  render: function () {
+    return (
+      <div className="about-page">
+        <About />
+        <Links />
+      </div>
+    );
+  }
+});
+
+module.exports = AboutPage;
