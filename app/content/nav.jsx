a7448c33eb00ad9023a795a8dc9c1323db8be9b3
Joshua Richey <joshua@idealist.org>
2015-07-05 20:07:02 -0700

    Fancy animated nav-toggle.

--- app/content/nav.jsx
+++ app/content/nav.jsx
@@ -25,23 +25,26 @@ var NavLinks = React.createClass({
 
 // Primary Nav //
 var Nav = React.createClass({
+  getInitialState: function() {
+      return {
+          on: false
+      };
+  },
+
   toggleNav() {
       store.isNavShowing = !store.isNavShowing;
+      this.setState({on: !this.state.on});
   },
   render() {
-    var style = {
-      color: '#fff',
-      width: '24px',
-      height: '24px'
-    };
+    var className = this.state.on ? "is-active" : "";
+    className += " nav-toggle";
+
     return (
       <nav className='site-nav'>
         <NavLinks />
-        <a className='nav-toggle' style={style} onClick={this.toggleNav} >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-289 381 32 32" enable-background="new -289 381 32 32">
-            <path className="menu-icon" d="M-261 386.9c.5 0 1 .5 1 1s-.5 1-1 1h-24c-.5 0-1-.5-1-1s.5-1 1-1h24m0-2h-24c-1.6 0-3 1.4-3 3s1.4 3 3 3h24c1.6 0 3-1.4 3-3s-1.4-3-3-3zM-261 396c.5 0 1 .5 1 1s-.5 1-1 1h-24c-.5 0-1-.5-1-1s.5-1 1-1h24m0-2h-24c-1.6 0-3 1.4-3 3s1.4 3 3 3h24c1.6 0 3-1.4 3-3s-1.4-3-3-3zM-261 405.1c.5 0 1 .5 1 1s-.5 1-1 1h-24c-.5 0-1-.5-1-1s.5-1 1-1h24m0-2h-24c-1.6 0-3 1.4-3 3s1.4 3 3 3h24c1.6 0 3-1.4 3-3s-1.4-3-3-3z"/>
-          </svg>
-        </a>
+        <button className={className} onClick={this.toggleNav}>
+          <span className='inner'>toggle menu</span>
+        </button>
       </nav>
     )
   }
