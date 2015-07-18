7e1b4fd5155ff43f5dde6fe74f34967c16180aef
Joshua Richey <joshua@idealist.org>
2015-07-18 14:06:14 -0700

    Added route for 404s.

--- app/routes.jsx
+++ app/routes.jsx
@@ -6,14 +6,17 @@ import Home from './content/home.jsx';
 import Art from './content/art.jsx';
 import Ig from './content/ig.jsx';
 import StyleGuide from './content/style-guide.jsx';
+import NotFound from './content/not_found.jsx';
 
 var Routes = (
-    <Route name="App" handler={App} path="/">
-        <DefaultRoute name="home" handler={Home} />
-        <Route name="guide" handler={StyleGuide} />
-        <Route name="art" handler={Art} />
-        <Route name="ig" handler={Ig} />
-    </Route>
-);
+  <Route name="App" handler={App} path="/">
+  <DefaultRoute name="home" handler={Home} />
+  <Route name="guide" handler={StyleGuide} />
+  <Route name="art" handler={Art} />
+  <Route name="ig" handler={Ig} />
+
+  <NotFoundRoute handler={NotFound} />
+  </Route>
+  );
 
 export default Routes;
