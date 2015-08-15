5badf708f3c75fcaf00a5625bd59305c26d7a986
Joshua Richey <joshua@idealist.org>
2015-08-15 12:34:09 -0700

    - add files -

---
+++ app/data/design.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'idealist',
+    description: `I’ve been working at <a href="http://idealist.org" target="_blank">idealist</a> for a few years now.`,
+    file: 'idealist-homepage.jpg'
+  },
+  {
+    title: 'another item',
+    description: ``,
+    file: 'sample.jpg'
+  }
+]
